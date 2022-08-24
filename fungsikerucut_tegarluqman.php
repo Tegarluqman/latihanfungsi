41763f7e7167c798c5a989c7457df898c7634b49
Tegarluqman <109190581+Tegarluqman@users.noreply.github.com>
2022-08-24 14:54:36 +0700

    Update fungsikerucut_tegarluqman.php

--- fungsikerucut_tegarluqman.php
+++ fungsikerucut_tegarluqman.php
@@ -5,5 +5,5 @@ $phi=22/7;
 function volumekerucut($r, $phi, $t){
           return (1/3*$phi*$r*$r*$t);
 }
-echo "volume balok tersebut adalah=" .volumekerucut($r, $phi, $t)."cm";
+echo "volume kerucut tersebut adalah=" .volumekerucut($r, $phi, $t)."cm";
 ?>
